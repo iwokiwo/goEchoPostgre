02bf8641e4590f3a106516b5b6e4831a41348f85
bayu <bayu@ipaymu.com>
2021-12-13 17:52:07 +0800

    product

--- models/products.go
+++ models/products.go
@@ -1,13 +1,16 @@
 package models
 
 type Products struct {
-	ID          int     `json:"id"`
-	Nama_produk string  `json:"nm_produk"`
+	ID               uint `json:"id" gorm:"size:36;not null;uniqueIndex;primary_key"`
+	Owner_id          int     `json:"owner_Id"`
+	Kategori_id int	`json:"kategori_id"`
+	Nama_produk string  `json:"nama_produk"`
 	Modal       float64 `json:"modal"`
 	Harga       float64 `json:"harga"`
-	Kategori_id int
-	Kategoris   Kategoris `gorm:"foreignKey:Kategori_id"`
-	Branch_id   int
+	Stok       float64 `json:"stok"`
+	Image string  `json:"image"`
+	Branch_id   int `json:"branch_id"`
 	Stores      Stores `gorm:"foreignKey:Branch_id"`
+	Kategoris   Kategoris `gorm:"foreignKey:Kategori_id"`
 	
 }
