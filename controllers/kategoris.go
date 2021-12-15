0c1478f28004263edad73032bd6dbfddf4a0f9de
bayu <bayu@ipaymu.com>
2021-12-15 17:54:30 +0800

    edit kategori

--- controllers/kategoris.go
+++ controllers/kategoris.go
@@ -2,7 +2,8 @@ package controllers
 
 import (
 	"net/http"
-
+	//"strconv"
+	"fmt"
 	"hotels/db"
 	"hotels/models"
 
@@ -36,17 +37,36 @@ func AddKategoris(c echo.Context) error {
 	
 		return c.JSON(http.StatusOK, err)
 	}
+	var yes uint=uint(jsonRegister["id"].(float64))
+		//y :=i
+	fmt.Println(yes)
+	if jsonRegister["id"] != nil {
+		var addkategori = models.Kategoris{
+			ID :yes,
+			User_id: idUser,
+			Nama_kategori: jsonRegister["nama_kategori"].(string),
+	
+		}
+		result := db.Save(&addkategori)
+		res.Status = http.StatusOK
+		res.Message = "Success"
+		res.Data = result.RowsAffected 
+		return c.JSON(http.StatusOK, res)
 
-	var addkategori = models.Kategoris{
-		User_id: idUser,
-		Nama_kategori: jsonRegister["nama_kategori"].(string),
-
+	} else {
+		var addkategori = models.Kategoris{
+			User_id: idUser,
+			Nama_kategori: jsonRegister["nama_kategori"].(string),
+	
+		}
+		//jika tidak ada simpan data
+		result := db.Create(&addkategori) 
+		res.Status = http.StatusOK
+		res.Message = "Success"
+		res.Data = result.RowsAffected 
+		return c.JSON(http.StatusOK, res)
 	}
-	//jika tidak ada simpan data
-	result := db.Create(&addkategori) 
-	res.Status = http.StatusOK
-	res.Message = "Success"
-	res.Data = result.RowsAffected 
-	return c.JSON(http.StatusOK, res)
+
+
 
 }
